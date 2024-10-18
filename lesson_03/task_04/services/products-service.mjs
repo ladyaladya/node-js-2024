5c5ab2bacb73df0d707654aa68534e799bb9671c
Vladyslav Vyshnivskyi <vladyslav.vyshnivskyi@avanquest.com>
2024-10-18 15:38:00 +0300

    Add images for products-list page

--- lesson_03/task_04/services/products-service.mjs
+++ lesson_03/task_04/services/products-service.mjs
@@ -1,21 +1,12 @@
 class InMemoryProductService {
   constructor() {
     this.products = [
-      { id: 1, name: 'Сухий корм для котів', category: 'Корм', price: 300, brand: 'Whiskas', country: 'Україна', forPets: 'Коти' },
-      { id: 2, name: 'Іграшка для собак', category: 'Іграшки', price: 150, brand: 'PetSafe', country: 'США', forPets: 'Собаки' },
-      { id: 3, name: 'Клітка для птахів', category: 'Аксесуари', price: 500, brand: 'AviaryPro', country: 'Німеччина', forPets: 'Птахи' },
-      { id: 4, name: 'Лежак для котів', category: 'Меблі', price: 200, brand: 'CatNap', country: 'Франція', forPets: 'Коти' },
-      { id: 5, name: 'Миска для собак', category: 'Аксесуари', price: 120, brand: 'DoggieStyle', country: 'Китай', forPets: 'Собаки' },
-      { id: 6, name: 'Грілка для тварин', category: 'Техніка', price: 350, brand: 'PetHeat', country: 'Японія', forPets: 'Усі' },
-      { id: 7, name: 'Туалет для котів', category: 'Аксесуари', price: 400, brand: 'LitterMatic', country: 'США', forPets: 'Коти' },
-      { id: 8, name: 'Ласощі для собак', category: 'Ласощі', price: 100, brand: 'BarkTreat', country: 'Німеччина', forPets: 'Собаки' },
-      { id: 9, name: 'Гребінець для котів', category: 'Гігієна', price: 80, brand: 'PetGroom', country: 'Україна', forPets: 'Коти' },
-      { id: 10, name: 'Корм для риб', category: 'Корм', price: 90, brand: 'AquaFeed', country: 'Китай', forPets: 'Риби' },
-      { id: 11, name: 'Шлея для собак', category: 'Аксесуари', price: 150, brand: 'WalkEasy', country: 'США', forPets: 'Собаки' },
-      { id: 12, name: 'Корм для гризунів', category: 'Корм', price: 200, brand: 'RodentDiet', country: 'Австралія', forPets: 'Гризуни' },
-      { id: 13, name: 'Зоомагазинна сумка', category: 'Меблі', price: 250, brand: 'PetBag', country: 'Франція', forPets: 'Усі' },
-      { id: 14, name: 'Вигул для собак', category: 'Аксесуари', price: 500, brand: 'PetWalk', country: 'Німеччина', forPets: 'Собаки' },
-      { id: 15, name: 'Гамак для птахів', category: 'Меблі', price: 200, brand: 'BirdNest', country: 'Канада', forPets: 'Птахи' }
+      { id: 1, name: 'Сухий корм для котів', category: 'Корм', price: 300, brand: 'Optimeal', country: 'Україна', forPets: 'Для дорослих котів', imagePath: 'optimeal.jpg' },
+      { id: 2, name: 'Optimeal Cat Mix', category: 'Вологий корм', price: 98, brand: 'Optimeal', country: 'Україна', forPets: 'Для дорослих котів, Для котів похилого віку', imagePath: 'group-optimeal.jpg' },
+      { id: 3, name: 'Rigor Cat Наповнювач', category: 'Наповнювачі для туалету', price: 399, brand: 'Rigor', country: 'Туреччина', forPets: 'Для котів', imagePath: 'rigor.jpg' },
+      { id: 4, name: 'Josera JosiDog', category: 'Сухий корм', price: 1109, brand: 'Josera', country: 'Німеччина', forPets: 'Для дорослих собак, Для собак похилого віку', imagePath: 'josera.jpg' },
+      { id: 5, name: 'Trixie М`ячк', category: 'Іграшки', price: 120, brand: 'Trixie', country: 'Німеччина, Китай', forPets: 'Для собак', imagePath: 'ball.jpg' },
+      { id: 6, name: 'Beaphar Care+', category: 'Корм', price: 554, brand: 'Beaphar', country: 'Нідерланди', forPets: 'Для кроликів', imagePath: 'beaphar-care.jpg' },
     ];
   }
 
